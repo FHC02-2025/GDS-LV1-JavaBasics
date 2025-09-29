b5f9d08e2efe73c7985bfe5a12b06f220c63aceb
Daniel Zettel <daniel.zettel@lv.campus02.at>
2025-09-29 19:16:32 +0200

    ArithmetischeOperationen

---
+++ src/ArithmetischeOperationen.java
@@ -0,0 +1,22 @@
+public class ArithmetischeOperationen {
+
+    public static void main(String[] args) {
+
+
+        // addieren -> +
+        // subtrahieren -> -
+        // multiplizieren -> *
+        // dividieren -> /
+        // modulo (Restwert) -> %
+
+        int a = 4;
+        int b = 3;
+
+        System.out.println(a + b); // STRG + D -> duplizieren
+        System.out.println(a - b); // STRG + D -> duplizieren
+        System.out.println(a * b); // STRG + D -> duplizieren
+        System.out.println(a / b); // STRG + D -> duplizieren
+        System.out.println(a % b); // STRG + D -> duplizieren
+
+    }
+}
