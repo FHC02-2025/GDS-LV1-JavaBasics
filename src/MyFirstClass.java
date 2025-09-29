2771f4fc5878906d5814c0ad52d4a27a1cd14754
Daniel Zettel <daniel.zettel@lv.campus02.at>
2025-09-29 17:35:52 +0200

    Kommentare hinzugefügt

--- src/MyFirstClass.java
+++ src/MyFirstClass.java
@@ -3,7 +3,20 @@ public class MyFirstClass {
     // psvm + tab
     // main + tab
     public static void main(String[] args) {
+
+        // Starte Programm hier -> Programm Start
+        System.out.println("Start");
+
+        // Logik
         System.out.println("My First Class");
+
+        // Zeilen verschieben
+        // STRG + Umschalt + Pfeil rauf/runter
+
+        // Ich bin ein Kommentar
+        System.out.println("Ende");
+
+        // Programm Ende
     }
 }
 
