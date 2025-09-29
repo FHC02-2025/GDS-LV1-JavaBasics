7eb806e027490df2979198de9bdf048175e6c383
Daniel Zettel <daniel.zettel@lv.campus02.at>
2025-09-29 19:08:12 +0200

    Calculator

---
+++ src/Calculator.java
@@ -0,0 +1,22 @@
+public class Calculator {
+
+    // psvm
+    public static void main(String[] args) {
+        // variablen deklarieren und initialisieren
+        int a = 2;
+        int b;
+        b = 3;
+        int result;
+        String name = "MyCalculator";
+
+        result = a + b;
+
+        // soutv + tab
+        System.out.println("name = " + name);
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("result = " + result);
+
+
+    }
+}
