f3ad22f09555cb7e1fa7e62c3841756b322389b0
Daniel Zettel <daniel.zettel@lv.campus02.at>
2025-10-03 16:14:52 +0200

    Calculator Demo angepasst

--- src/CalculatorExcercise.java
+++ src/CalculatorExcercise.java
@@ -6,23 +6,31 @@ public class CalculatorExcercise {
         int number1 = 10;
         int number2 = 2;
 
+        startCalculator(number1, number2);
+    }
+
+    private static void startCalculator(int number1, int number2) {
         int result = add(number1, number2);
-        printResult(number1, number2, result);
+
+        printResult(number1, number2, result, "Addition");
 
         result = subtract(number1, number2);
-        printResult(number1, number2, result);
+        printResult(number1, number2, result, "Subraktion");
 
         result = multiply(number1, number2);
-        printResult(number1, number2, result);
+        printResult(number1, number2, result, "Multiplikation");
 
         double resultFromDivide = divide(number1, number2);
         System.out.println("resultFromDivide = " + resultFromDivide);
     }
 
-    public static void printResult(int number1, int number2, int result) {
+
+    public static void printResult(int number1, int number2, int result, String methodName) {
+        System.out.println("Resultat der " + methodName);
         System.out.println("Input 1 = " + number1);
         System.out.println("Input 2 = " + number2);
         System.out.println("Resultat = " + result);
+        System.out.println();
     }
 
     public static int add(int a, int b) {
