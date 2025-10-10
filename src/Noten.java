a25e695c9b24f41c8a951df2c73acc261cd31079
Daniel Zettel <daniel.zettel@lv.campus02.at>
2025-10-10 17:43:34 +0200

    arrays beispiel

--- src/Noten.java
+++ src/Noten.java
@@ -5,10 +5,16 @@ public class Noten {
         // 1 Array
         // 5 Fächer
         // 5 Noten
-        // Erstellen array mit Noten aus allen Fächern
+        // Erstellen sie ein array mit Noten aus allen Fächern
         // geben sie anschließend alle Noten auf der Konsole aus
 
+        // array mit noten initialisieren
+        int[] noten = {1,1,2,1,2};
 
+        // zugriff über for
+        for (int i = 0; i < noten.length; i++) {
+            System.out.println("Note: " + noten[i]);
+        }
 
     }
 }
